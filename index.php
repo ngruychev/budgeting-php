c183080e0787b7afe7ad98eb013b5dd43a137adc
Nikolay Gruychev <ngruychev@gmail.com>
2022-09-18 15:37:02 +0200

    feat: better homepage

--- index.php
+++ index.php
@@ -14,5 +14,18 @@ require_once "util/require_login.php";
 <body>
   <?php require_once "component/header.php"; ?>
   <h1>Welcome, <?php echo htmlspecialchars($CURRENT_USER->username) ?></h1>
+  <p>Here you can track your spending and earnings (under Transactions), and see statistics (under Stats)</p>
+  <p>Click on the menu to get started</p>
+  <p>Or, use these shortcuts:</p>
+  <ul>
+    <li><a href="transactions.php" class="btn btn-sm btn-a">Transactions</a></li>
+    <li><a href="stats.php" class="btn btn-sm btn-a">Stats</a></li>
+    <li><a href="edit_transaction?new" class="btn btn-sm btn-b">Add transaction</a></li>
+  </ul>
+  <p>
+    On Chrome for Android, you can click the three dots in the top right corner,
+    and then "Add to homescreen" to add an icon to your homescreen that will open this app.
+    On Android 7.1 and higher, you can long-press the icon to see a list of shortcuts directly from your homescreen.
+  </p>
 </body>
 </html>
